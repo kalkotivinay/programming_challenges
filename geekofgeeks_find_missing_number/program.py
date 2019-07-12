22781d2ee6ca29a2f8aaf155ba6a2bbaff7e6ac2
kalkotivinay <kalkoti.vinay@gmail.com>
2019-07-12 09:23:34 +0000

    geekofgeeks: find missing number
    Signed-off-by: kalkotivinay <kalkoti.vinay@gmail.com>

---
+++ geekofgeeks_find_missing_number/program.py
@@ -0,0 +1,12 @@
+# getMissingNo takes list as argument
+def getMissingNo(A):
+    n = len(A)
+    total = (n + 1)*(n + 2)/2
+    sum_of_A = sum(A)
+    return total - sum_of_A
+
+# Driver program to test above function
+rinput = raw_input()
+ilist = rintput.split(' ')
+miss = getMissingNo(ilist)
+print(miss)
